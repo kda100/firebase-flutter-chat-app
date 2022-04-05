7d8158db9ea71a8b9966af4cc734f7393bcc3b19
kda100 <65980399+kda100@users.noreply.github.com>
2022-04-05 22:55:15 +0100

    Update main.dart

--- lib_ios_and_android/main.dart
+++ lib_ios_and_android/main.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:io';
 import 'package:firebase_chat_app/providers/chat_provider.dart';
 import 'package:firebase_chat_app/screens/chat_screen.dart';
-import 'package:firebase_chat_app/constants/text_styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -26,92 +25,91 @@ Future<void> main() async {
 class FirebaseFlutterChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final home = ChangeNotifierProvider<ChatProvider>(
-      create: (_) => ChatProvider(),
-      child: ChatScreen(),
-    );
     return KeyboardDismissOnTap(
       //Keyboard dismisses on tap anywhere in app.
-      child: Platform.isIOS
-          ? CupertinoApp(
-              home: home,
-              theme: CupertinoThemeData(
-                primaryColor: ColorPalette.primaryColor,
-                barBackgroundColor: ColorPalette.darkPrimaryColor,
-                scaffoldBackgroundColor: ColorPalette.primaryBackgroundColor,
-                textTheme: CupertinoTextThemeData(
-                  primaryColor: ColorPalette.primaryTextColor,
-                  navTitleTextStyle: TextStyle(
-                    fontFamily: Fonts.fontFamily,
-                    color: ColorPalette.secondaryTextColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
+      child: ChangeNotifierProvider<ChatProvider>(
+        create: (_) => ChatProvider(),
+        child: Platform.isIOS
+            ? CupertinoApp(
+                home: ChatScreen(),
+                theme: CupertinoThemeData(
+                  primaryColor: ColorPalette.primaryColor,
+                  barBackgroundColor: ColorPalette.darkPrimaryColor,
+                  scaffoldBackgroundColor: ColorPalette.primaryBackgroundColor,
+                  textTheme: CupertinoTextThemeData(
+                    primaryColor: ColorPalette.primaryTextColor,
+                    navTitleTextStyle: TextStyle(
+                      fontFamily: Fonts.fontFamily,
+                      color: ColorPalette.secondaryTextColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
+                    textStyle: TextStyle(
+                        fontFamily: Fonts.fontFamily,
+                        color: ColorPalette.primaryTextColor,
+                        fontSize: 15),
+                    actionTextStyle: TextStyle(
+                        fontFamily: Fonts.fontFamily,
+                        color: ColorPalette.primaryColor,
+                        fontSize: 17),
                   ),
-                  textStyle: TextStyle(
+                ),
+                localizationsDelegates: GlobalMaterialLocalizations.delegates,
+                supportedLocales: [
+                  const Locale('en'),
+                ],
+              )
+            : MaterialApp(
+                home: ChatScreen(),
+                theme: ThemeData(
+                  fontFamily: Fonts.fontFamily,
+                  colorScheme: Theme.of(context).colorScheme.copyWith(
+                        primary: ColorPalette.primaryColor,
+                        secondary: ColorPalette.accentColor,
+                        background: ColorPalette.primaryBackgroundColor,
+                      ),
+                  primaryColor: ColorPalette.primaryColor,
+                  primaryColorDark: ColorPalette.darkPrimaryColor,
+                  primaryColorLight: ColorPalette.lightPrimaryColor,
+                  dialogTheme: DialogTheme(
+                    backgroundColor: ColorPalette.primaryBackgroundColor,
+                  ),
+                  backgroundColor: ColorPalette.primaryBackgroundColor,
+                  cardTheme: CardTheme(
+                    color: ColorPalette.primaryBackgroundColor,
+                    elevation: 10,
+                  ),
+                  progressIndicatorTheme: ProgressIndicatorThemeData(
+                    color: ColorPalette.primaryColor,
+                  ),
+                  bottomSheetTheme: BottomSheetThemeData(
+                    backgroundColor: ColorPalette.primaryBackgroundColor,
+                  ),
+                  appBarTheme: AppBarTheme(
+                    color: ColorPalette.darkPrimaryColor,
+                    titleTextStyle: TextStyle(
+                      fontSize: 20,
+                      fontFamily: Fonts.fontFamily,
+                    ),
+                  ),
+                  iconTheme: IconThemeData(
+                    color: ColorPalette.primaryIconColor,
+                  ),
+                  textTheme: TextTheme(
+                    bodyText2: TextStyle(
                       fontFamily: Fonts.fontFamily,
                       color: ColorPalette.primaryTextColor,
-                      fontSize: 15),
-                  actionTextStyle: TextStyle(
-                      fontFamily: Fonts.fontFamily,
-                      color: ColorPalette.primaryColor,
-                      fontSize: 17),
-                ),
-              ),
-              localizationsDelegates: GlobalMaterialLocalizations.delegates,
-              supportedLocales: [
-                const Locale('en'),
-              ],
-            )
-          : MaterialApp(
-              home: home,
-              theme: ThemeData(
-                fontFamily: Fonts.fontFamily,
-                colorScheme: Theme.of(context).colorScheme.copyWith(
-                      primary: ColorPalette.primaryColor,
-                      secondary: ColorPalette.accentColor,
-                      background: ColorPalette.primaryBackgroundColor,
+                      fontSize: 14,
                     ),
-                primaryColor: ColorPalette.primaryColor,
-                primaryColorDark: ColorPalette.darkPrimaryColor,
-                primaryColorLight: ColorPalette.lightPrimaryColor,
-                dialogTheme: DialogTheme(
-                  backgroundColor: ColorPalette.primaryBackgroundColor,
-                ),
-                backgroundColor: ColorPalette.primaryBackgroundColor,
-                cardTheme: CardTheme(
-                  color: ColorPalette.primaryBackgroundColor,
-                  elevation: 10,
-                ),
-                progressIndicatorTheme: ProgressIndicatorThemeData(
-                  color: ColorPalette.primaryColor,
-                ),
-                bottomSheetTheme: BottomSheetThemeData(
-                  backgroundColor: ColorPalette.primaryBackgroundColor,
-                ),
-                appBarTheme: AppBarTheme(
-                  color: ColorPalette.darkPrimaryColor,
-                  titleTextStyle: TextStyle(
-                    fontSize: 20,
-                    fontFamily: Fonts.fontFamily,
                   ),
+                  scaffoldBackgroundColor: ColorPalette.primaryBackgroundColor,
                 ),
-                iconTheme: IconThemeData(
-                  color: ColorPalette.primaryIconColor,
-                ),
-                textTheme: TextTheme(
-                  bodyText2: TextStyle(
-                    fontFamily: Fonts.fontFamily,
-                    color: ColorPalette.primaryTextColor,
-                    fontSize: 14,
-                  ),
-                ),
-                scaffoldBackgroundColor: ColorPalette.primaryBackgroundColor,
+                localizationsDelegates: GlobalMaterialLocalizations.delegates,
+                supportedLocales: [
+                  const Locale('en'),
+                ],
               ),
-              localizationsDelegates: GlobalMaterialLocalizations.delegates,
-              supportedLocales: [
-                const Locale('en'),
-              ],
-            ),
+      ),
     );
   }
 }
