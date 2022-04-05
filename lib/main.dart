d809f3a5cf7c3beba61a2ce99b0a3577728cded6
kda100 <65980399+kda100@users.noreply.github.com>
2022-04-05 22:49:15 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,50 +25,50 @@ class FirebaseFlutterChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return KeyboardDismissOnTap(
       //Keyboard dismisses on tap anywhere in app.
-      child: MaterialApp(
-        title: Strings.appName,
-        theme: ThemeData(
-            fontFamily: "Arvo",
-            colorScheme: Theme.of(context).colorScheme.copyWith(
-                  primary: ColorPalette.primaryColor,
-                  secondary: ColorPalette.accentColor,
-                  background: ColorPalette.primaryBackgroundColor,
+      child: ChangeNotifierProvider<ChatProvider>(
+        create: (context) => ChatProvider(),
+        child: MaterialApp(
+          title: Strings.appName,
+          theme: ThemeData(
+              fontFamily: "Arvo",
+              colorScheme: Theme.of(context).colorScheme.copyWith(
+                    primary: ColorPalette.primaryColor,
+                    secondary: ColorPalette.accentColor,
+                    background: ColorPalette.primaryBackgroundColor,
+                  ),
+              primaryColor: ColorPalette.primaryColor,
+              primaryColorDark: ColorPalette.darkPrimaryColor,
+              primaryColorLight: ColorPalette.lightPrimaryColor,
+              textTheme: TextTheme(
+                bodyText1: TextStyle(
+                  color: ColorPalette.primaryTextColor,
                 ),
-            primaryColor: ColorPalette.primaryColor,
-            primaryColorDark: ColorPalette.darkPrimaryColor,
-            primaryColorLight: ColorPalette.lightPrimaryColor,
-            textTheme: TextTheme(
-              bodyText1: TextStyle(
-                color: ColorPalette.primaryTextColor,
+                bodyText2: TextStyle(
+                  color: ColorPalette.secondaryTextColor,
+                ),
               ),
-              bodyText2: TextStyle(
-                color: ColorPalette.secondaryTextColor,
+              dialogTheme: DialogTheme(
+                backgroundColor: ColorPalette.primaryBackgroundColor,
               ),
-            ),
-            dialogTheme: DialogTheme(
               backgroundColor: ColorPalette.primaryBackgroundColor,
-            ),
-            backgroundColor: ColorPalette.primaryBackgroundColor,
-            cardTheme: CardTheme(
-              color: ColorPalette.primaryBackgroundColor,
-              elevation: 10,
-            ),
-            progressIndicatorTheme: ProgressIndicatorThemeData(
-              color: ColorPalette.primaryColor,
-            ),
-            bottomSheetTheme: BottomSheetThemeData(
-              backgroundColor: ColorPalette.primaryBackgroundColor,
-            ),
-            appBarTheme: AppBarTheme(color: ColorPalette.darkPrimaryColor),
-            iconTheme: IconThemeData(color: ColorPalette.primaryIconColor),
-            scaffoldBackgroundColor: ColorPalette.primaryBackgroundColor),
-        localizationsDelegates: GlobalMaterialLocalizations.delegates,
-        supportedLocales: [
-          const Locale('en'),
-        ],
-        home: ChangeNotifierProvider<ChatProvider>(
-          create: (context) => ChatProvider(),
-          child: ChatScreen(), //displays chat screen.
+              cardTheme: CardTheme(
+                color: ColorPalette.primaryBackgroundColor,
+                elevation: 10,
+              ),
+              progressIndicatorTheme: ProgressIndicatorThemeData(
+                color: ColorPalette.primaryColor,
+              ),
+              bottomSheetTheme: BottomSheetThemeData(
+                backgroundColor: ColorPalette.primaryBackgroundColor,
+              ),
+              appBarTheme: AppBarTheme(color: ColorPalette.darkPrimaryColor),
+              iconTheme: IconThemeData(color: ColorPalette.primaryIconColor),
+              scaffoldBackgroundColor: ColorPalette.primaryBackgroundColor),
+          localizationsDelegates: GlobalMaterialLocalizations.delegates,
+          supportedLocales: [
+            const Locale('en'),
+          ],
+          home: ChatScreen(), //displays chat screen.
         ),
       ),
     );
