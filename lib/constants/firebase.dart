ca410642f95e78fed2fa03415f30ce59058fb6d4
kda100 <65980399+kda100@users.noreply.github.com>
2022-04-25 08:37:02 +0100

    Add files via upload

---
+++ lib/constants/firebase.dart
@@ -0,0 +1,21 @@
+const chatItemColRefPath = "chatItems";
+
+const String recipientDocRefPath = "users/4321";
+const String userDocRefPath = "users/1234";
+
+const String imagesStorageRefPath = "images";
+const String videosStorageRefPath = "videos";
+
+const String videoFileStoragePath = "video";
+const String thumbnailStoragePath = "thumbnail";
+
+class FieldNames {
+  static const String nameField = "name";
+  static const String chatItemTypeField = "chatItemType";
+  static const String sentByIdField = "sentById";
+  static const String contentField = "content";
+  static const String createdAtField = "createdAt";
+  static const String isRecipientField = "isRecipient";
+  static const String onCloudField = "onCloud";
+  static const String readField = "read";
+}
